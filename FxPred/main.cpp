2376357c4a2c4e2a08470992e4ce131d16872b67
BusinessAlcon <businessalcon@gmail.com>
2020-11-12 18:56:09 +0100

    Initial Commit

---
+++ FxPred/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FxPred
+//
+//  Created by Liam Briegel on 12.11.20.
+//  Copyright © 2020 Liam Briegel. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
